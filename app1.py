196557cc0a301acfa03b91e9adde58f83fd2f25c
bhuvanadmsc <bhuvanad.msc@gmail.com>
2025-10-06 23:03:06 +0530

    prediction added

---
+++ app1.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from joblib import load
+app = Flask(__name__)
+# Load saved model
+model = load('sentiment_model.joblib')
+@app.route('/predict', methods=['POST'])
+def predict():
+ data = request.get_json()
+ review_text = [data['review']]
+ prediction = model.predict(review_text)[0]
+ sentiment = "Positive" if prediction == 1 else "Negative"
+ return jsonify({'sentiment': sentiment})
+if __name__ == '__main__':
+ app.run(debug=True)
